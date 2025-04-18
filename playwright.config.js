96aa5a350875f70d84eafe6fcc36796714f6d87a
IrynaDim <i.dymytreyeva@gmail.com>
2025-04-18 18:57:17 +0300

    add possibility to choose browser and run several workers

--- playwright.config.js
+++ playwright.config.js
@@ -1,8 +1,33 @@
 import { defineConfig } from '@playwright/test';
 
+const viewportWidth = parseInt(process.env.VIEWPORT_WIDTH || '1280', 10);
+const viewportHeight = parseInt(process.env.VIEWPORT_HEIGHT || '720', 10);
+const threadCount = parseInt(process.env.THREADS || '2', 10); // по умолчанию 2
+
 export default defineConfig({
-    timeout: 30000, // ⏱️ общий таймаут для теста (включая все действия)
+    timeout: 30000,
     expect: {
-        timeout: 15000, // ⏱️ таймаут для expect(locator).toBeVisible(), toHaveText() и т.п.
+        timeout: 15000,
     },
+    workers: threadCount,
+    use: {
+        viewport: {
+            width: viewportWidth,
+            height: viewportHeight,
+        },
+    },
+    projects: [
+        {
+            name: 'chromium',
+            use: { browserName: 'chromium' },
+        },
+        {
+            name: 'firefox',
+            use: { browserName: 'firefox' },
+        },
+        {
+            name: 'webkit',
+            use: { browserName: 'webkit' },
+        },
+    ],
 });
